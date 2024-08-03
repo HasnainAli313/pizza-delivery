aad735f1d3ef8a4902c784dcbf9b2f67d57b3f4d
HasnainAli313 <aliorakzai407@gmail.com>
2024-08-03 23:07:25 +0500

    completed Cart component

--- src/components/FoodMenu.jsx
+++ src/components/FoodMenu.jsx
@@ -5,9 +5,13 @@ function FoodMenu() {
   return (
     <section>
         <h3 className='text-center'>Food Menu</h3>
-        <div className='food-menu d-flex'>
-          <Cart foodTitle="Pizza" price="$2.5" />
-          <Cart foodTitle="shorma" price="$3.6" />
+        <div className='food-menu d-flex flex-wrap'>
+          <Cart foodTitle="Margherita" price="$2.5" />
+          <Cart foodTitle="Pizzette Montanare" price="$3.6" />
+          <Cart foodTitle="Quattro Formaggi" price="$4.6" />
+          <Cart foodTitle="Diavola" price="$5.6" />
+          <Cart foodTitle="California-style Pizza" price="$4.6" />
+          <Cart foodTitle="Miami Pizza" price="$4.6" />
         </div>
     </section>
   )
