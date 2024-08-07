eaed35b92e122ca06403f09117c8b4da627fb3d1
Hasnain Ali <93475112+HasnainAli313@users.noreply.github.com>
2024-08-07 15:32:42 +0500

    updated

--- src/components/ExploreFoods.jsx
+++ src/components/ExploreFoods.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import img1 from "../assets/pizza1.jpg"
+import img2 from "../assets/pizza2.jpg"
+import img3 from "../assets/pizza3.jpg"
 
 function ExploreFoods() {
   return (
@@ -6,12 +9,18 @@ function ExploreFoods() {
     <section>
         <h3 className='text-center p-5 '>Explore Foods</h3>
     <div className='explore-food d-flex flex-wrap gap-3 justify-content-around p-3  '>
-       <div className=' text-center text-white rounded-3 pizza1 '>
-        <p className='position-relative  mt-auto end-0'>Pizza</p></div>
-       <div className=' text-center text-white rounded-3 pizza2 '>
-        <p className='position-relative  mt-auto end-0'>Pizza</p></div>
-       <div className=' text-center text-white rounded-3 pizza3 '>
-        <p className='position-relative  mt-auto end-0'>Pizza</p></div>
+       <div className=' text-center text-white rounded-3 pizza '>
+        <img src={img1} alt="" />
+        <p className='para'>Pizza</p>
+        </div>
+       <div className=' text-center text-white rounded-3 pizza '>
+       <img src={img2} alt="" />
+        <p className='para'>Pizza</p>
+        </div>
+       <div className=' text-center text-white rounded-3 pizza '>
+       <img src={img3} alt="" />
+        <p className='para'>Pizza</p>
+        </div>
     </div>
     </section>
     </>
